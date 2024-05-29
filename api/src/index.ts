96f5012115845dc49084a73a836e0ed8828f85c0
phillip-steven <putu.phillip@gmail.com>
2024-05-29 14:03:08 +0800

    fix(connection between fe and be)

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import path from "path";
-import dotenv from "dotenv";
-import bodyParser = require("body-parser");
-const cors = require("cors");
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import bodyParser = require('body-parser');
+const cors = require('cors');
 
 dotenv.config({
-  path: path.resolve(__dirname, "../.env"),
+	path: path.resolve(__dirname, '../.env'),
 });
 
 const app = express();
@@ -15,42 +15,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
-  cors({
-    origin: process.env.WHITELISTED_DOMAIN,
-  })
+	cors({
+		origin: process.env.WHITELISTED_DOMAIN,
+	}),
 );
 
 const port: number = Number(process.env.PORT) || 8000;
 
-const categoryRoute = require("./route/productCategoryRoute");
-app.use("/category", categoryRoute);
+const categoryRoute = require('./route/productCategoryRoute');
+app.use('/category', categoryRoute);
 
-const transactionRoute = require("./route/transactionRoute");
-app.use("/transaction", transactionRoute);
+const transactionRoute = require('./route/transactionRoute');
+app.use('/transaction', transactionRoute);
 
-const cartRoute = require("./route/cartRoute");
-app.use("/cart", cartRoute);
+const cartRoute = require('./route/cartRoute');
+app.use('/cart', cartRoute);
 
-const transactionDetailRoute = require("./route/transactionDetailRoutes");
-app.use("/transaction-detail", transactionDetailRoute);
+const transactionDetailRoute = require('./route/transactionDetailRoutes');
+app.use('/transaction-detail', transactionDetailRoute);
 
-const productRoute = require("./route/productRoute");
-app.use("/product", productRoute);
+const productRoute = require('./route/productRoute');
+app.use('/product', productRoute);
 
-const branchRoute = require("./route/branchRoute");
-app.use("/branch", branchRoute);
+const branchRoute = require('./route/branchRoute');
+app.use('/branch', branchRoute);
 
-const statusRoute = require("./route/statusRoute");
-app.use("/status", statusRoute);
+const statusRoute = require('./route/statusRoute');
+app.use('/status', statusRoute);
 
-const profileRoute = require("./route/profileImageRoute");
-app.use("/profile", profileRoute);
+const profileRoute = require('./route/profileImageRoute');
+app.use('/profile', profileRoute);
 
-app.use("/uploads", express.static(path.join(__dirname, "./public/images")));
+app.use('/uploads', express.static(path.join(__dirname, './public/images')));
 
-const stockRoute = require("./route/stockRoute");
-app.use("/stock", stockRoute);
+const stockRoute = require('./route/stockRoute');
+app.use('/stock', stockRoute);
 
 app.listen(port, () => {
-  console.log(`server started on port ${port}`);
+	console.log(`Server started on port : [${port}]`);
 });
