6b07c9673598347dcd85961c3966e274b2c9247b
phillip-steven <putu.phillip@gmail.com>
2024-05-30 14:22:12 +0800

    fix(router)
    create router with create browser router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,53 +1,11 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Transaction from "./pages/Transaction";
-import { AdminView } from "./pages/AdminView";
-import { ErrorPages } from "./pages/ErrorPages";
-import { ProductList } from "./component/ProductList";
-import { Text } from "@chakra-ui/react";
-import { Inventory } from "./pages/Inventory";
-import { AdminProductList } from "./pages/AdminView/AdminProductList";
-import { Category } from "./pages/AdminView/Category";
-import { Report } from "./pages/AdminView/Report";
-import { Dashboard } from "./pages/Dashboard";
+import { Outlet } from 'react-router-dom';
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="transaction" element={<Transaction />} />
-      <Route path="/admin" element={<AdminView />} />
-      <Route
-        path="/admin/dashboard"
-        element={<AdminView view={<Dashboard />} />}
-      />
-      <Route
-        path="/admin/report"
-        element={<AdminView view={<Report />} />}
-      />
-      <Route
-        path="/admin/dashboard"
-        element={<AdminView view={<ProductList />} />}
-      />
-      <Route
-        path="/admin/report"
-        element={<AdminView view={<Text>Report</Text>} />}
-      />
-      <Route
-        path="/admin/inventory"
-        element={<AdminView view={<Inventory />} />}
-      />
-      <Route
-        path="/admin/product"
-        element={<AdminView view={<AdminProductList />} />}
-      />
-      <Route
-        path="/admin/category"
-        element={<AdminView view={<Category />} />}
-      />
-      <Route path="*" element={<ErrorPages />} />
-    </Routes>
-  );
+import { Box } from '@chakra-ui/react';
+
+export default function App() {
+	return (
+		<Box maxW={'100vw'} maxH={'100vh'}>
+			<Outlet />
+		</Box>
+	);
 }
-
-export default App;
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,15 +1,24 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
-import theme from "./theme/theme.ts";
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { ChakraProvider, Text } from '@chakra-ui/react';
+import {
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from 'react-router-dom';
+import theme from './theme/theme.ts';
 
-ReactDOM.createRoot(
-  document.getElementById("root")!
-).render(
-  <BrowserRouter>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </BrowserRouter>
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<App />}>
+			<Route path='/dashboard' element={<Text>Test</Text>} />
+		</Route>,
+	),
+);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<ChakraProvider theme={theme}>
+		<RouterProvider router={router} />
+	</ChakraProvider>,
 );
