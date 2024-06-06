de8be0ffaa6cbac4f52f2e3587fb714a70dbd717
phillip-steven <putu.phillip@gmail.com>
2024-06-06 16:55:29 +0800

    fix(auth-login)
    add jwt token return when login

--- api/src/service/authService.ts
+++ api/src/service/authService.ts
@@ -1,6 +1,8 @@
 import bcrypt from 'bcrypt';
 import { PrismaClient } from '@prisma/client';
 import { UserLogin } from '../query/authQuery';
+import jwt, { Secret } from 'jsonwebtoken';
+import { exclude } from '../utils/excludePassword';
 
 const prisma = new PrismaClient();
 
@@ -11,8 +13,21 @@ export const loginService = async ({ email, password }: UserLogin) => {
 		});
 		if (!isUserExist) throw new Error('Sorry, email doesnt exist');
 		const isValid = await bcrypt.compare(password, isUserExist.password);
+
 		if (!isValid) throw new Error('Wrong password');
-		return 'Login Success';
+		let payload = {
+			id: isUserExist.id,
+			email: isUserExist.email,
+			roleId: isUserExist.role_id,
+		};
+		const jwtSecretKey = process.env.JWT_SECRET_KEY;
+		if (!jwtSecretKey) {
+			throw new Error('JWT_SECRET_KEY is not defined in environment variables');
+		}
+		const token = jwt.sign(payload, jwtSecretKey, {
+			expiresIn: '1h',
+		});
+		return { user: exclude(isUserExist, ['password']), token };
 	} catch (err) {
 		throw err;
 	}
