ac970316e0976ff8381a5b97fd63ffbea836faef
putuphillipsteven <putu.phillip@gmail.com>
2024-10-03 11:21:45 +0700

    fix(migration)
    remove avatar and product group table

---
+++ api/src/prisma/migrations/20241003042120_remove/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `avatar` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `product_group` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `avatar`;
+
+-- DropTable
+DROP TABLE `product_group`;
